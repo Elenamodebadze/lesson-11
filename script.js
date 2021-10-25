0a9afec7a11f76e2c33d51d703bd4d74fe12f4ca
Elenamodebadze <91465218+Elenamodebadze@users.noreply.github.com>
2021-10-25 16:42:26 +0400

    update script.js

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
+// Example one
 let user = {
-    firstname: ‘giorgi’,
-    lastname: ‘smith,
+    firstname: "giorgi",
+    lastname: "smith",
     age: 25,
-   studentstatus: active
-  };
-  
+    studentstatus:"active"};
+
+console.log(user.studentstatus);
+
+//Example two
+
+let array1 = ["giorgi", "smith", 25, "passive"];
+
+for (let x =0; x < array1.length; x++) {
+  console.log (array1[x]);}
+
+//Example three 
+
+let numbers = [5, 6, 10, 25, 3, 7, 4, 100, 28, 36, 89];
+
+console.log(numbers[2]);
