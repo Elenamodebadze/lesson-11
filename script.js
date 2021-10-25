edd889169f75a34a9d1f44cf97ec4ff24d9e45e0
Elenamodebadze <91465218+Elenamodebadze@users.noreply.github.com>
2021-10-25 18:38:18 +0400

    update script.js

--- script.js
+++ script.js
@@ -1,21 +1,56 @@
-// Example one
-let user = {
-    firstname: "giorgi",
-    lastname: "smith",
-    age: 25,
-    studentstatus:"active"};
+// // Example one
+// let user = {
+//     firstname: "giorgi",
+//     lastname: "smith",
+//     age: 25,
+//     studentstatus:active};
 
-console.log(user.studentstatus);
+// console.log(user.studentstatus);
 
-//Example two
 
-let array1 = ["giorgi", "smith", 25, "passive"];
 
-for (let x =0; x < array1.length; x++) {
-  console.log (array1[x]);}
+// //Example two.1
+// let array1 = ["giorgi", "smith", 25, "passive"];
+
+// for (let x =0; x < array1.length; x++) {
+//   console.log (array1[x]);};
+
+// Example two.2
+// let item = 0;
+// while (item < array1.length){
+//   console.log(array1[item]);
+//   item++;}
+
 
 //Example three 
+// let numbers = [5, 6, 10, 25, 3, 7, 4, 100, 28, 36, 89];
 
-let numbers = [5, 6, 10, 25, 3, 7, 4, 100, 28, 36, 89];
+// for (let x=0; x < numbers.length; x++){
+//   console.log(numbers[x]);}
 
-console.log(numbers[2]);
+// Example five
+
+// let array = ["html", "css", "bootstrap", "javascript","python"];
+
+// for (let x=0; x < array.length; x++) {
+//   if (array[x].length > 5 && array[x].includes("ava")){
+//     console.log(array[x]);
+//   }}
+
+
+// Example six
+
+let user = {
+  name: "giorgi",
+  age:  20,
+  studentstatus: "active"}
+
+      if (user[age] < 18 && user[studentstatus].includes ("active")) {
+        console.log ("hello");
+      }
+  
+
+
+
+
+
