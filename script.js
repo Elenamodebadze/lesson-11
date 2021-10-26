be4795b4e6f3a5293e2d8e1df88f73f6b901d838
Elenamodebadze <91465218+Elenamodebadze@users.noreply.github.com>
2021-10-26 23:10:34 +0400

    update script.js

--- script.js
+++ script.js
@@ -1,54 +1,70 @@
 // // Example one
-// let user = {
-//     firstname: "giorgi",
-//     lastname: "smith",
-//     age: 25,
-//     studentstatus:active};
+let user = {
+    firstname: "giorgi",
+    lastname: "smith",
+    age: 25,
+    studentstatus:"active"};
 
-// console.log(user.studentstatus);
+console.log(user.studentstatus);
 
 
 
 // //Example two.1
-// let array1 = ["giorgi", "smith", 25, "passive"];
+let array1 = ["giorgi", "smith", 25, "passive"];
 
-// for (let x =0; x < array1.length; x++) {
-//   console.log (array1[x]);};
+for (let i =0; i < array1.length; i++) {
+  console.log (array1[i])}
 
 // Example two.2
-// let item = 0;
-// while (item < array1.length){
-//   console.log(array1[item]);
-//   item++;}
+let i = 0;
+while (i < array1.length){
+
+  console.log(array1[i]);
+  i++;}
 
 
 //Example three 
-// let numbers = [5, 6, 10, 25, 3, 7, 4, 100, 28, 36, 89];
+let numbers = [5, 6, 10, 25, 3, 7, 4, 100, 28, 36, 89];
 
-// for (let x=0; x < numbers.length; x++){
-//   console.log(numbers[x]);}
+for (let i=0; i < numbers.length; i++){
+  if (numbers[i] > 5)
 
-// Example five
+  console.log (numbers[i])
+}
+  
 
-// let array = ["html", "css", "bootstrap", "javascript","python"];
+// Example four
 
-// for (let x=0; x < array.length; x++) {
-//   if (array[x].length > 5 && array[x].includes("ava")){
-//     console.log(array[x]);
-//   }}
+let array = ["html", "css", "bootstrap", "javascript","python"];
+
+for (let y=0; y < array.length; y++) {
+  if (array[y].length > 5 && array[y].includes("ava"))
+    console.log(array[y]);
+  
+}
 
 
 // Example six
 
-let user = {
+let users = {
   name: "giorgi",
   age:  20,
   studentstatus: "active"}
 
-      if (user[age] < 18 && user[studentstatus].includes ("active")) {
-        console.log ("hello");
-      }
-  
+ if (user.age>18 && user.studentstatus==="active") {
+   console.log("hello")
+ }
+
+
+if (users.name==="giorgi"){
+  console.log("hello giorgi")
+}
+
+if(users.studentstatus=="active" || users.age<25){
+  console.log ("hello world")
+}
+
+
 
 
 
